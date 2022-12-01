1a78fdfdb43bc1ae1fed2d1192df3a0ca4d9c0ca
dimaproba <103955606+dimaproba@users.noreply.github.com>
2022-12-01 09:11:07 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,51 @@
+'use strict'
+//Первая домашка
+
+// Первое видео
+let lesson22 = 'Я учу JS';
+console.log (lesson22);
+
+// Варик 1
+console.log (lesson22);
+// Варик 2
+console.log ('JS');
+console.log ('Я учу');
+// Второе видео
+// Первое задание
+let userEyeColor = 'green';
+// Второе задание
+let userName = 'Вася';
+let user;
+user = userName;
+console.log(user);
+// Третье задание
+let js = 38; 
+let js1 = 8;
+console.log (js)
+console.log (js1)
+
+// Четвертое задание
+const BLOCK_SIZE = 14 + 50;
+// Правильно
+const blockSize = 14 + 50;
+
+// Третье видео
+
+// Первое задание
+let userAge = 36;
+let userInfo = `Фрилансер: ${userAge}`;
+console.log (userInfo);
+
+// Второе задание
+let userHeight = 145 / 0;
+console.log (userHeight);
+console.log (typeof userHeight);
+
+// Третье задание
+let userName1;
+console.log (typeof userName1);
+
+// Четвертое задание
+let userSize = "45" / "8";
+console.log (typeof userSize);
+
